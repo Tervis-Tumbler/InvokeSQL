b5da013cbda0923c952d45b20fe9a8f049ebbad0
ChrisMagnuson <hourglasssand@gmail.com>
2018-03-17 13:53:11 -0400

    Add functions to support MSSQL, refactor ODBC to be DRY, moved code from InvokeSQLAnywhereSQL to this module

--- InvokeSQL.psm1
+++ InvokeSQL.psm1
@@ -30,23 +30,39 @@
     }
 }
 
+function ConvertTo-MSSQLConnectionString {
+    param (
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$Server,
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$Database,
+        $Credential = [System.Management.Automation.PSCredential]::Empty
+    )
+    process {
+        New-MSSQLConnectionString @PSBoundParameters
+    }
+}
+
+function New-MSSQLConnectionString {
+    param (
+        [Parameter(Mandatory)]$Server,
+        [Parameter(Mandatory)]$Database,
+        $Credential = [System.Management.Automation.PSCredential]::Empty
+    )
+    if ($Credential -ne [System.Management.Automation.PSCredential]::Empty) {
+        "Server=$Server;Database=$Database;User Id=$($Credential.UserName);Password=$($Credential.GetNetworkCredential().password);"
+    } else {
+        "Data Source=$Server; Integrated Security=SSPI; Initial Catalog=$Database"
+    }
+}
+
+
 function Invoke-SQLODBC {
     param (
         [string]$DataSourceName,
-        [string]$SQLCommand = $(throw "Please specify a query.")
+        [string]$SQLCommand = $(throw "Please specify a query."),
+        [Switch]$ConvertFromDataRow
     )
     $ConnectionString = "DSN=$DataSourceName"
-
-    $Connection = new-object System.Data.Odbc.OdbcConnection($ConnectionString)
-    $Command = new-object System.Data.Odbc.OdbcCommand($SQLCommand,$Connection)
-    $Connection.Open()
-    
-    $Adapter = New-Object System.Data.Odbc.OdbcDataAdapter $Command
-    $Dataset = New-Object System.Data.DataSet
-    $Adapter.Fill($Dataset) | Out-Null
-    
-    $Connection.Close()
-    $DataSet.Tables 
+    Invoke-SQLGeneric -ConnectionString $ConnectionString -SQLCommand $SQLCommand -DatabaseEngineClassMapName ODBC -ConvertFromDataRow:$ConvertFromDataRow
 }
 
 function ConvertFrom-DataRow {
@@ -71,3 +87,115 @@ function ConvertFrom-DataRow {
     }
 }
 
+function Invoke-MSSQL {
+    param(
+        [Parameter(Mandatory,ParameterSetName="NoConnectionString")]$Server,
+        [Parameter(Mandatory,ParameterSetName="NoConnectionString")]$Database,
+        [Parameter(ParameterSetName="NoConnectionString")]$Credential = [System.Management.Automation.PSCredential]::Empty,
+        [Parameter(Mandatory,ParameterSetName="ConnectionString")][string]$ConnectionString,
+        [Parameter(Mandatory)][string]$SQLCommand,
+        [Switch]$ConvertFromDataRow
+    )
+    if (-not $ConnectionString) {
+        $ConnectionString = New-MSSQLConnectionString -Server $Server -Database $Database -Credential $Credential
+        Invoke-SQLGeneric -ConnectionString $ConnectionString -SQLCommand $SQLCommand -ConvertFromDataRow:$ConvertFromDataRow -DatabaseEngineClassMapName MSSQL
+    } else {
+        Invoke-SQLGeneric -DatabaseEngineClassMapName MSSQL @PSBoundParameters
+    }
+}
+
+function Install-InvokeSQLAnywhereSQL {
+    choco install sqlanywhereclient -version 12.0.1 -y
+}
+
+function ConvertTo-SQLAnywhereConnectionString {
+    param(
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)][string]$Host,
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)][string]$DatabaseName,
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)][string]$ServerName,
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)][string]$UserName,
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)][string]$Password
+    )
+    "UID=$UserName;PWD=$Password;Host=$Host;DatabaseName=$DatabaseName;ServerName=$ServerName"
+}
+
+$DatabaseEngineClassMap = [PSCustomObject][Ordered]@{
+    Name = "SQLAnywhere"
+    NameSpace = "iAnywhere.Data.SQLAnywhere"
+    Connection = "SAConnection"
+    Command = "SACommand"
+    Adapter = "SADataAdapter"
+    AddTypeScriptBlock = {Add-iAnywhereDataSSQLAnywhereType}
+},
+[PSCustomObject][Ordered]@{
+    Name = "Oracle"
+    NameSpace = "Oracle.ManagedDataAccess.Client"
+    Connection = "OracleConnection"
+    Command = "OracleCommand"
+    Adapter = "OracleDataAdapter"
+    AddTypeScriptBlock = {Add-OracleManagedDataAccessType}
+},
+[PSCustomObject][Ordered]@{
+    Name = "MSSQL"
+    NameSpace = "system.data.sqlclient"
+    Connection = "SQLConnection"
+    Command = "SQLCommand"
+    Adapter = "SQLDataAdapter"
+},
+[PSCustomObject][Ordered]@{
+    Name = "ODBC"
+    NameSpace = "System.Data.Odbc"
+    Connection = "OdbcConnection"
+    Command = "OdbcCommand"
+    Adapter = "OdbcDataAdapter"
+}
+
+
+function Get-DatabaseEngineClassMap {
+    param (
+        $Name
+    )
+    $DatabaseEngineClassMap | where Name -EQ $Name
+}
+
+function Add-iAnywhereDataSSQLAnywhereType {
+    Add-Type -AssemblyName "iAnywhere.Data.SQLAnywhere, Version=12.0.1.36052, Culture=neutral, PublicKeyToken=f222fc4333e0d400"
+}
+
+function Invoke-SQLGeneric {
+    param(
+        [Parameter(Mandatory)][string]$ConnectionString,
+        [Parameter(Mandatory)][string]$SQLCommand,
+        [Parameter(Mandatory)][ValidateSet("SQLAnywhere","Oracle","MSSQL","ODBC")]$DatabaseEngineClassMapName,
+        [Switch]$ConvertFromDataRow
+    )
+    $ClassMap = Get-DatabaseEngineClassMap -Name $DatabaseEngineClassMapName
+    $NameSpace = $ClassMap.NameSpace
+    if ($ClassMap.AddTypeScriptBlock) { & $ClassMap.AddTypeScriptBlock }
+
+    $Connection = New-Object -TypeName "$NameSpace.$($ClassMap.Connection)" $ConnectionString
+    $Command = New-Object "$NameSpace.$($ClassMap.Command)" $SQLCommand,$Connection
+    $Connection.Open()
+    
+    $Adapter = New-Object "$NameSpace.$($ClassMap.Adapter)" $Command
+    $Dataset = New-Object System.Data.DataSet
+    $Adapter.Fill($DataSet) | Out-Null
+    
+    $Connection.Close()
+    
+    if ($ConvertFromDataRow) {
+        $DataSet.Tables | ConvertFrom-DataRow
+    } else {
+        $DataSet.Tables
+    }
+}
+
+function Invoke-SQLAnywhereSQL {
+    param(
+        [Parameter(Mandatory)][string]$ConnectionString,
+        [Parameter(Mandatory)][string]$SQLCommand,
+        [ValidateSet("SQLAnywhere","Oracle","MSSQL")]$DatabaseEngineClassMapName = "SQLAnywhere",
+        [Switch]$ConvertFromDataRow
+    )
+    Invoke-SQLGeneric @PSBoundParameters
+}
